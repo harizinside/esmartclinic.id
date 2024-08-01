1efde74c704aef9dc9fccd7cc759690a3294cf96
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2024-08-01 17:25:50 +0700

    add list menu

--- server/api/v1/oauth/sign-in.post.ts
+++ server/api/v1/oauth/sign-in.post.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod'
+import { Types } from 'mongoose'
 import { compareSync } from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { UserModel } from '@/server/models/users.schema'
+import { PrevilageModel } from '~/server/models/privilages.schema'
 
 export default defineEventHandler(async (event) => {
   const userSchema = z.object({
@@ -41,3 +43,105 @@ export default defineEventHandler(async (event) => {
 
   return { status: true, token: token, user: user }
 })
+
+const getMenusByUserId = async (userId: string) => {
+  try {
+    const result = await PrevilageModel.aggregate([
+      { $match: { 'users.userId': new Types.ObjectId(userId) } },
+      { $unwind: '$menu' },
+      {
+        $lookup: {
+          from: 'menus',
+          localField: 'menu.menuId',
+          foreignField: '_id',
+          as: 'menuDetails',
+        },
+      },
+      { $unwind: '$menuDetails' },
+      {
+        $unwind: {
+          path: '$menuDetails.children',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: 'menus',
+          localField: 'menuDetails.children._id',
+          foreignField: '_id',
+          as: 'childMenuDetails',
+        },
+      },
+      {
+        $unwind: {
+          path: '$childMenuDetails',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          menuId: '$menu.menuId',
+          name: '$menuDetails.name',
+          icon: '$menuDetails.icon',
+          status: '$menuDetails.status',
+          order: '$menuDetails.order',
+          updatedAt: '$menuDetails.updatedAt',
+          permissions: {
+            create: '$menu.create',
+            read: '$menu.read',
+            update: '$menu.update',
+            delete: '$menu.delete',
+            import: '$menu.import',
+            export: '$menu.export',
+          },
+          children: {
+            $cond: {
+              if: { $gt: [{ $size: '$menu.children' }, 0] },
+              then: {
+                $map: {
+                  input: '$menu.children',
+                  as: 'child',
+                  in: {
+                    name: '$childMenuDetails.name',
+                    icon: '$childMenuDetails.icon',
+                    status: '$childMenuDetails.status',
+                    order: '$childMenuDetails.order',
+                    createdAt: '$childMenuDetails.createdAt',
+                    updatedAt: '$childMenuDetails.updatedAt',
+                    permissions: {
+                      create: '$$child.create',
+                      read: '$$child.read',
+                      update: '$$child.update',
+                      delete: '$$child.delete',
+                      import: '$$child.import',
+                      export: '$$child.export',
+                    },
+                  },
+                },
+              },
+              else: [],
+            },
+          },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          menus: { $push: '$$ROOT' },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          menus: 1,
+        },
+      },
+    ])
+
+    return result
+  }
+  catch (error) {
+    return error
+  }
+}
