301168c194eaeea097d9c0161b0f80965f8b81f2
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2024-08-01 14:55:34 +0700

    add privilages schema to project

---
+++ server/models/privilages.schema.ts
@@ -0,0 +1,58 @@
+import type { Document, Types } from 'mongoose'
+import { Schema } from 'mongoose'
+import { MenuModel } from './menus.schema'
+import { UserModel } from './users.schema'
+import { defineMongooseModel } from '#nuxt/mongoose'
+
+interface IPrevilage extends Document {
+  name: string
+  category: 'dashboard' | 'users'
+  menu: {
+    menuId: Types.ObjectId
+    create: boolean
+    read: boolean
+    update: boolean
+    delete: boolean
+    import: boolean
+    export: boolean
+  }[]
+  users: {
+    userId: Types.ObjectId
+    status: boolean
+  }[]
+  group: string
+  updatedAt: Date
+}
+
+const PrevilageSchema = new Schema<IPrevilage>(
+  {
+    name: { type: String },
+    category: { type: String, enum: ['dashboard', 'users'] },
+    menu: [{
+      menuId: { type: 'ObjectId', ref: MenuModel, required: true },
+      create: { type: Boolean, required: true, default: false },
+      read: { type: Boolean, required: true, default: false },
+      update: { type: Boolean, required: true, default: false },
+      delete: { type: Boolean, required: true, default: false },
+      import: { type: Boolean, required: true, default: false },
+      export: { type: Boolean, required: true, default: false },
+    }],
+    users: [{
+      userId: { type: UserModel, required: true },
+      status: { type: Boolean },
+    }],
+    group: { type: String },
+    updatedAt: { type: Date },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+)
+
+export const PrevilageModel = defineMongooseModel<IPrevilage>({
+  name: 'previlages',
+  schema: PrevilageSchema,
+})
+
+export { IPrevilage, PrevilageSchema }
